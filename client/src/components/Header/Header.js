6e40c1ad7fabae984558ee18bb8e469ce3b64c23
Stefan Fredriksson <sf222pv@student.lnu.se>
2020-08-13 17:31:15 +0200

    Adding react

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header () {
+  return (
+    <header>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+    </header>
+  )
+}
